c654428f37e624287e6e259c001e8e3e7bd3a944
Jean-Philippe Trinh <jptrinh@antidot.net>
2023-10-01 09:51:28 +0200

    initialize transform

---
+++ .github/workflows/transform-script.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const inputFilePath = process.env.INPUT_JSON_FILE;
+const outputFilePath = process.env.OUTPUT_JSON_FILE;
+
+try {
+  const inputData = JSON.parse(fs.readFileSync(inputFilePath, 'utf8'));
+  
+  // Your transformation logic here
+  
+  const transformedData = inputData; // Replace with your transformation logic
+
+  fs.writeFileSync(outputFilePath, JSON.stringify(transformedData, null, 2));
+
+  console.log(`JSON transformation complete. Output saved to ${outputFilePath}`);
+} catch (error) {
+  console.error('Error transforming JSON:', error);
+  process.exit(1);
+}
